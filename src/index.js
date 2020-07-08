1789193f8084a18734ed4a6974092500bdcb56c2
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2020-07-08 09:49:06 +0700

    v1.0.4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 export {use} from './use'
-export * from './uses/i18n'
-export * from './uses/router'
-export * from './uses/vuex'
+export {
+    i18Use,
+    LocaleManager,
+} from './uses/i18n'
+export {
+    routerUse,
+    Middleware,
+    CommonMiddleware,
+    PromiseMiddleware,
+    SessionMiddleware,
+} from './uses/router'
+export {vuexUse} from './uses/vuex'
